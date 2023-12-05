e55c0eb4dd74beb3d945e9a3657791e2d74a8f81
Aiirinn <149657451+Aiirinn@users.noreply.github.com>
2023-12-05 22:43:15 +0300

    Add files via upload

---
+++ lab2/Model/SortedBook.cs
@@ -0,0 +1,8 @@
+namespace ConsoleApp2.Model;
+
+public class SortedBook
+{
+    public string? Title { get; set; }
+    public int Counter { get; set; }
+    public bool IsKeywordInAnnotation = false;
+}
