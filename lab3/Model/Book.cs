2f0b793f0a4e1a248a8125922a15da503a0260e3
Aiirinn <149657451+Aiirinn@users.noreply.github.com>
2023-12-08 20:52:51 +0300

    Update Book.cs

--- lab3/Model/Book.cs
+++ lab3/Model/Book.cs
@@ -8,7 +8,7 @@ public class Book
     public int Id { get; set; }
     public string Title { get; set; }
     public string Author { get; set; }
-    public string Genre { get; set; }
+    public List<string> Genres { get; set; }
     public DateTime PublicationDate { get; set; }
     public string Annotation { get; set; }
     public string ISBN { get; set; }
